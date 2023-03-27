eb57e8ab30355f9365844f5b4ed2ed2d25993799
K-Magano <124664588+K-Magano@users.noreply.github.com>
2023-03-27 14:05:37 +0200

    fixed comment mistakes

---
+++ IWA_02  Challenge Solutions/Challenge 1/script.js
@@ -0,0 +1,12 @@
+/* This is the firstname of the user */
+//In both examples, the * was on the wrong side of the forward slash.
+
+const user = 'John'
+
+
+/* This is the lastname of the user */ 
+
+const surname = 'Smith';
+
+//we separate the const using commas not semi-colons 
+console.log(user, surname)
