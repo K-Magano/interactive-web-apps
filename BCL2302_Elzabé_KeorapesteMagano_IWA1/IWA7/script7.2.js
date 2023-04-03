67216d1b783306f7fb5decbab51238642cbd0a71
K-Magano <124664588+K-Magano@users.noreply.github.com>
2023-04-03 18:46:58 +0200

    attempting challenge 7

--- BCL2302_Elzabé_KeorapesteMagano_IWA1/IWA7/script7.2.js
+++ BCL2302_Elzabé_KeorapesteMagano_IWA1/IWA7/script7.2.js
@@ -1,17 +1,29 @@
-const nickname = "Timmy"
-const firstname = "Timothy" 
-
-if (nickname === "Timmy"){
-console.log (`Good Morning, ${nickname} !` )
+// const nickname = "Timmy"
+// const firstname = "Timothy" 
+// //try const user = nickname || (firstname) || 'good Morning
+// if (nickname === "Timmy"){
+// console.log (`Good Morning, ${nickname} !` )
     
-} else if (firstname === "Timothy")
-{
-console.log (`Good Morning, ${firstname} !` )
+// } else if (firstname === "Timothy")
+// {
+// console.log (`Good Morning, ${firstname} !` )
     
-} else(nickname && firstname === " ")
-{
-    console.log(`Good Morning`)
-}
-//console.log(`Good Morning, ${nickname} || ${firstname}!`) //logs 'Good Morning, Timmy ||Timothy!'
+// } else(nickname && firstname === " ")
+// {
+//     console.log(`Good Morning`)
+// }
+// //console.log(`Good Morning, ${nickname} || ${firstname}!`) //logs 'Good Morning, Timmy ||Timothy!'
 
-//using interpolation because its a string, used template literals to indicate it's a string
+// //using interpolation because its a string, used template literals to indicate it's a string
+
+
+const nickname =  ' '
+const firstname = ' '  
+const user = nickname || firstname 
+
+{console.log(`Good Morning! ${user}`)}
+
+if (user = ' ' )
+    {console.log(`Good Morning!`)}
+  
+// Two ways, but second worked better because of || and fallback variables and throws and syntax //! error
--- BCL2302_Elzabé_KeorapesteMagano_IWA1/IWA7/script7.3.js
+++ BCL2302_Elzabé_KeorapesteMagano_IWA1/IWA7/script7.3.js
@@ -1,19 +1,37 @@
 const leoName = 'Leo'
-const leoSurname = 'Musvaire     '
+const leoSurname = 'Musvaire'
 const leoBalance = '-9394'
 
-const sarahName = 'Sarah    '
+const sarahName = 'Sarah'
 const sarahSurname = 'Kleinhans'
 const sarahBalance = '-4582.21000111'
 
 const divider = '----------------------------------'
 
 // Only change below this line
+const owed = parseInt( leoBalance*-1 + sarahBalance*-1)
+const leo = (`${leoName} ${leoSurname}  ${leoBalance*-1}`)
 
-const owed = parseInt('R' + leoBalance + sarahBalance)
-const leo = "{leoName} + {leoSurname} + \"Owed\" + \"R\" + {sarahBalance}"
-const sarah = "{leoName} + {surname} + \"Owed\" + \"R\" + {sarahBalance}"
-const total = "Total amount owed: "
-const result = leo + sarah + divider + divider + total + owed + divider
+const sarah = (`${sarahName} ${sarahSurname} ${sarahBalance*-1}`)
+const total = (`Total amount: ${owed}: `)
+const result = (`${leo}  ${sarah} ${owed}  ${divider}`)
 
 console.log(result)
+
+/*const total = (owed)
+const resultS = (`${leo} ${sarah} ${divider}  ${owed} ${divider}`)
+const result = (owed)
+console.log(result.toFixed(2))* 
+* only gave the number 
+ / 
+
+//!Expected OutCome
+
+/*Leo Musvaire (Owed: R 9394.00)
+Sarah Kleinhans (Owed: R 4582.20)
+
+----------------------------------
+  Total amount owed: R 14 976.20
+----------------------------------*/
+
+//could have ${total} and still got "Total amount owed:" but Total amount owed: ${owed}  still works
