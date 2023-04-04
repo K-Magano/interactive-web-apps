9d4cfc3f71dd45fb53526565b44c256bcf9727f8
K-Magano <124664588+K-Magano@users.noreply.github.com>
2023-04-04 13:13:41 +0200

    completed challenge 9

--- BCL2302_Elzabé_KeorapesteMagano_IWA1/IWA9/Script9.js
+++ BCL2302_Elzabé_KeorapesteMagano_IWA1/IWA9/Script9.js
@@ -8,6 +8,8 @@ const size = 'large'
 const expenses = {
     food: 51.7501,
     transport:  10.2,
+
+
 }
   
 const tax = {
@@ -19,20 +21,23 @@ const tax = {
 }
 
 const rent = {
-    none: 0,
-    'small-room': 200,
-    'large-room': 300,
-    'small-apartment': 400,
-    'large-apartment': 800,
-    'small-house': 1200,
-    'large-house': 2400,
-}
+     none: 0,
+        'small-room': 200,
+        'small-house': 1200,
+        'small-apartment': 400,
+          'large-room': 300,
+          'large-apartment': 800,
+          'large-house': 2400,
+   }
 
 // You can change below however you want
-
-const taxAsDecimal = tax.913 / 100
-const startingAfterTax = salary * 1 - taxAsDecimal
+const taxAsDecimal = 480 //parseInt(tax[913]) / 100 //tax is 12%
+const startingAfterTax = salary  - taxAsDecimal
 const type = lodging + size
-const balance = expenses(transport) - expenses(food) - expenses(rent.type) 
-console.log(balance)
- 
+let rooms = rent[`large-apartment`] //so i can get a number
+const balance = (startingAfterTax - rooms - expenses.food - expenses.transport)
+//const balance = (`${rent} ${type} ${expenses.food + expenses.transport}`) = [object Object] apartment  large 61.950100000000006
+console.log(`R ${balance.toFixed(2)}`)
+
+
+ //!2685.25
