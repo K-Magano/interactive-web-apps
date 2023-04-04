d211aaac200b95f5bcbff5b37aa5830d8576e8b5
K-Magano <124664588+K-Magano@users.noreply.github.com>
2023-04-04 14:18:47 +0200

    completed challenge6

--- BCL2302_Elzabé_KeorapesteMagano_IWA1/IWA6/script6.2.js
+++ BCL2302_Elzabé_KeorapesteMagano_IWA1/IWA6/script6.2.js
@@ -9,21 +9,14 @@ const minuteOfDay = 00;
 // Only change below this line
 
 const expenses = transport + food + rent
-const startingAfterTax = salary *(1 - parseInt(tax) / 100 )
+const taxAsDecimal = 96 //const startingAfterTax = salary - taxAsDecimal*(1 - parseInt(tax) / 100 ) to do it the other way 
+const startingAfterTax = salary  - taxAsDecimal
 
-
-if (hourOfDay !==null && minuteOfDay !== null) {
-console.log("⚠️Error")}
-
+if (hourOfDay !==null && minuteOfDay !== null) {console.log("⚠️Error")}
 else (hourOfDay === 00 && minuteOfDay === 00) 
-
-{   
-    const balance = startingAfterTax - expenses
-
-   }
+    const balance = startingAfterTax - expenses 
    
-   console.log(`R ${balance.toFixed(2)}`)// my salary is 4000
+   console.log(`R ${balance.toFixed(2)}`)
 
 
-
-  
+  //!My Value R 242.05
